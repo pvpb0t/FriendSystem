8b44b2a3d0dc83631a2cff4bcd3ec53925bc803b
pvpb0t <74259011+pvpb0t@users.noreply.github.com>
2022-06-17 19:08:07 +0200

    Create FriendObj.java

---
+++ friend/FriendObj.java
@@ -0,0 +1,18 @@
+package me.pvpb0t.callisto.api.friend;
+
+public class FriendObj {
+
+    private String uuid;
+
+    public FriendObj(){
+
+    }
+
+    public FriendObj(String uuid){
+        this.uuid = uuid;
+    }
+
+    public String getUuid() {
+        return this.uuid;
+    }
+}
