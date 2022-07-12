816816279d1e02c4c432855fef3593c7d7591335
pvpb0t <74259011+pvpb0t@users.noreply.github.com>
2022-07-12 14:33:41 +0200

    Create FriendConfig.java

---
+++ friend/FriendConfig.java
@@ -0,0 +1,69 @@
+package me.pvpb0t.callisto.api.friend;
+
+import me.pvpb0t.callisto.Callisto;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import java.io.*;
+import java.util.ArrayList;
+
+public class FriendConfig extends Thread{
+
+    static FriendManager friendManager = Callisto.getCallisto().getFriendManager();
+    static String path = Callisto.mainFolder + File.separator +  "friends.json";
+
+    @Override
+    public void run(){
+        saveFriendsToJSON();
+    }
+
+    public static void loadFriendsFromJSON() {
+        JSONParser parser = new JSONParser();
+        ArrayList<FriendObj> loadedFriends = new ArrayList<>();
+        FriendObj friend = new FriendObj();
+        String fileName = path;
+        if(new File(path).exists()) {
+
+            try {
+
+                Object obj = parser.parse(new FileReader(fileName));
+                JSONObject jsonObject = (JSONObject) obj;
+
+                JSONArray UUIDs = (JSONArray) jsonObject.get("UUID");
+                for (Object uuid : UUIDs) {
+                    friend.setUUID((String) uuid);
+                    loadedFriends.add(friend);
+                }
+
+            } catch (FileNotFoundException e) {
+                System.err.println("ERROR : FILE NOT FOUND.");
+            } catch (IOException | ParseException e) {
+                throw new RuntimeException(e);
+            }
+            if (!loadedFriends.isEmpty()) {
+                friendManager.setFriends(loadedFriends);
+            }
+        }
+    }
+
+    public void saveFriendsToJSON(){
+        JSONObject jsonObject = new JSONObject();
+        JSONArray array = new JSONArray();
+
+        for(FriendObj friend : friendManager.getFriends()){
+            array.add(friend.getUuid());
+        }
+        jsonObject.put("UUID", array);
+        try{
+            FileWriter file = new FileWriter(path);
+            file.write(jsonObject.toJSONString());
+            file.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+}
