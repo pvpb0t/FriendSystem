f12e07231cfaea35f52d780cb39759ee82d719e2
pvpb0t <74259011+pvpb0t@users.noreply.github.com>
2022-07-12 14:34:21 +0200

    Update List.java

--- command/List.java
+++ command/List.java
@@ -2,6 +2,7 @@ package me.pvpb0t.callisto.impl.command;
 
 import me.pvpb0t.callisto.Callisto;
 import me.pvpb0t.callisto.api.command.Command;
+import me.pvpb0t.callisto.api.friend.FriendManager;
 import me.pvpb0t.callisto.api.friend.FriendObj;
 import me.pvpb0t.callisto.api.util.Wrapper;
 import me.pvpb0t.callisto.api.util.logger.Logger;
@@ -16,10 +17,18 @@ public class List extends Command implements Wrapper {
     @Override
     public void onTrigger(String arguments)
     {
-
+        FriendManager friendManager = Callisto.getCallisto().getFriendManager();
+        if(friendManager.getFriends().size() <= 0){
+            Logger.getLogger().printToChat("You have no friends lmao");
+        }
 
         for(FriendObj friendObj : Callisto.getCallisto().getFriendManager().getFriends()){
-            Logger.getLogger().printToChat(Callisto.getCallisto().getFriendManager().getName(friendObj.getUuid()));
+            FriendManager.getName(friendObj.getUuid()).thenAccept(s->{
+
+                    Logger.getLogger().printToChat(s);
+
+            });
+            //Logger.getLogger().printToChat(friendManager.getName(friendObj.getUuid()));
         }
 
     }
