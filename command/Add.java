9e6ffc5319ffb7d69835abe6a313a0d8f9b25de6
pvpb0t <74259011+pvpb0t@users.noreply.github.com>
2022-07-12 14:33:57 +0200

    Update Add.java

--- command/Add.java
+++ command/Add.java
@@ -1,5 +1,3 @@
-package me.pvpb0t.callisto.api.command;
-
 package me.pvpb0t.callisto.impl.command;
 
 import me.pvpb0t.callisto.Callisto;
@@ -8,7 +6,10 @@ import me.pvpb0t.callisto.api.friend.FriendManager;
 import me.pvpb0t.callisto.api.util.Wrapper;
 import me.pvpb0t.callisto.api.util.logger.Logger;
 
+import java.util.concurrent.atomic.AtomicBoolean;
+
 public class Add extends Command implements Wrapper {
+    AtomicBoolean shouldreturn = new AtomicBoolean(false);
 
     public Add()
     {
@@ -18,6 +19,7 @@ public class Add extends Command implements Wrapper {
     @Override
     public void onTrigger(String arguments)
     {
+        shouldreturn.set(false);
 
         if (arguments.equals(""))
         {
@@ -29,20 +31,34 @@ public class Add extends Command implements Wrapper {
 
         if(arguments.length() > 22){
             //means its a uuid
-            if(friendManager.getName(arguments) == "invalid name"){
-                Logger.getLogger().printToChat("Player not found");
-                return;
+            FriendManager.getName(arguments).thenAccept(s->{
+                if(s.equals("invalid name")){
+                    Logger.getLogger().printToChat("Player not found");
+                    shouldreturn.set(true);
+                }
+            });
+            //if(friendManager.getName(arguments).equals("invalid name")){
+              //  Logger.getLogger().printToChat("Player not found");
+                //return;
+            //}
+            if(!shouldreturn.get()){
+                friendManager.addFriend(arguments, true);
+                Logger.getLogger().printToChat("Added: " + friendManager.getName(arguments));
             }
-            Callisto.getCallisto().getFriendManager().addFriend(arguments, true);
-            Logger.getLogger().printToChat("Added: " + Callisto.getCallisto().getFriendManager().getName(arguments));
         }else{
             //means its a playername
-            if(friendManager.getUuid(arguments) == "invalid name"){
-                Logger.getLogger().printToChat("Player not found");
-                return;
+            FriendManager.getUuid(arguments).thenAccept(s->{
+                if(s.equals("invalid name")){
+                    Logger.getLogger().printToChat("Player not found");
+                    shouldreturn.set(true);
+                }
+            });
+
+            if(!shouldreturn.get()){
+                friendManager.addFriend(arguments, false);
+                Logger.getLogger().printToChat("Added: " + arguments);
             }
-            Callisto.getCallisto().getFriendManager().addFriend(arguments, false);
-            Logger.getLogger().printToChat("Added: " + arguments);
+
         }
 
     }
